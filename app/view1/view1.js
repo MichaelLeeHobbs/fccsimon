a280a75ebf90090be3a5cd04d82ffdd57ce6ed34
Michael Hobbs <michael.lee.hobbs@gmail.com>
2015-09-18 10:00:29 -0400

    on off switch wired and animated

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -2,14 +2,27 @@
 
 angular.module('myApp.view1', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  });
-}])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/', {
+            templateUrl: 'view1/view1.html',
+            controller: 'View1Ctrl'
+        });
+    }])
 
-.controller('View1Ctrl', ['$scope', function($scope) {
-      $scope.count = '- -';
+    .controller('View1Ctrl', ['$scope', function ($scope) {
+        var simon = {
+            on: false
 
-}]);
+        };
+
+        var updateView = function () {
+            $scope.onOff = simon.on;
+
+        };
+        $scope.count = '- -';
+        $scope.switchClick = function () {
+            simon.on = !simon.on;
+            updateView();
+        };
+
+    }]);
