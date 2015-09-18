ff9bdf4553b8734aef30e939c32210a2704e1c9d
Michael Hobbs <michael.lee.hobbs@gmail.com>
2015-09-18 10:47:30 -0400

    updated counter to change color when on

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -11,26 +11,38 @@ angular.module('myApp.view1', ['ngRoute'])
 
     .controller('View1Ctrl', ['$scope', function ($scope) {
         var simon = {
+            toggleOnOff: function () {
+                this.on = !this.on;
+                if (!this.on) {
+                    this.strict = false;
+                    this.count = '- -';
+                }
+            },
+            toggleStrict: function () {
+                if (this.on) {
+                    this.strict = !this.strict;
+                }
+            },
             on: false,
-            strict: false
+            strict: false,
+            count: '- -'
 
         };
 
         var updateView = function () {
             $scope.onOff = simon.on;
             $scope.strictLed = simon.strict;
-
+            $scope.count = simon.count;
         };
-        $scope.count = '- -';
         $scope.switchClick = function () {
-            simon.on = !simon.on;
+            simon.toggleOnOff();
             updateView();
         };
         $scope.strictClick = function () {
-            if (simon.on) {
-                simon.strict = !simon.strict;
-                updateView();
-            }
+            simon.toggleStrict();
+            updateView();
         };
 
+
+        updateView();
     }]);
